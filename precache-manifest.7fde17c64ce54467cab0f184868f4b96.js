23c7e56bde3b6b1a94924e1b39683b84cd87a011
Full Stack Web Developer <khaledhossionmv@gmail.com>
2019-12-31 20:00:08 +0600

    Add files via upload

---
+++ precache-manifest.7fde17c64ce54467cab0f184868f4b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927826e6f812f5e4953b2ed620715757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a22936ec40d3f56a2c7",
+    "url": "/static/css/main.f5a9e365.chunk.css"
+  },
+  {
+    "revision": "c64fbda19e0d5ecd2195",
+    "url": "/static/js/2.01553eef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.01553eef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a22936ec40d3f56a2c7",
+    "url": "/static/js/main.e9133e30.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
